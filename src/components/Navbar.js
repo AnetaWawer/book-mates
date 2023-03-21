2a4a312fd144f63cf43da8e2b0c4d4ff0319af21
jkuca <46653059+jkuca@users.noreply.github.com>
2023-03-21 11:53:14 +0100

    Add icons. Navbar looks almost like in mockup. Searching animation doesn't work.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,32 +2,57 @@ import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import "../style.css"
 import "../normalize.css"
-// import "../navbar.css"
-
+import "../vendor.css"
+import "../icomoon.css"
 
 function Navbar() {
     return (
-        <nav className="nav">
-                <Link className="site-logo" to="/">Logo</Link>
+        <div id="header-wrap">
+            <header id="header">
+                <div className="container">
+                    <div className="row">
 
-            <ul>
-                <li>
-                    <Link to="/">Konto</Link>
-                </li>
-                <li>
-                    <Link to="/">Książki</Link>
-                </li>
-                <li>
-                    <Link to="/">Wydarzenia</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Kontakt</Link>
-                </li>
-                <li>
-                    <Link to="/">Lupa</Link>
-                </li>
-            </ul>
-        </nav>
+                        <div className="col-md-2">
+                            <div className="main-logo">
+                                <p>BOOKMATES</p>
+                            </div>
+
+                        </div>
+
+                        <div className="col-md-10">
+                            <nav id="navbar">
+                                <div className="main-menu stellarnav">
+                                    <ul className="menu-list">
+                                        <li className="menu-item active"></li><a href="#" className="user-account for-buy"><i
+                                                className="icon icon-user"></i>Konto</a>
+                                            <li className="menu-item "><Link to="/books" className="nav-link" data-effect="Books">Książki</Link></li>
+                                            <li className="menu-item "><Link to="/" className="nav-link" data-effect="Wydarzenia">Wydarzenia</Link></li>
+                                            <div className="action-menu">
+
+                                                <div className="search-bar">
+                                                    <a href="#" className="search-button search-toggle"
+                                                       data-selector="#header-wrap">
+                                                        <i className="icon icon-search"></i>
+                                                    </a>
+                                                    <form role="search" method="get" className="search-box">
+                                                        <input className="search-field text search-input"
+                                                               placeholder="Search" type="search"></input>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            </ul>
+                                            <div className="hamburger">
+                                                <span className="bar"></span>
+                                                <span className="bar"></span>
+                                                <span className="bar"></span>
+                                            </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
     );
 }
 
