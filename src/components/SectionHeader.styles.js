86a8903c7d56b206685e28d4af5fc37382ce5210
Aneta Wawer <anwawer1@gmail.com>
2023-03-23 10:26:26 +0100

    section header created

---
+++ src/components/SectionHeader.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Header = styled.div `
+    position: relative;
+ 
+`
+export const Title = styled.header `
+  font-size: 2em;
+  line-height: 1;
+  position: relative;
+  margin-bottom: 75px;
+  text-align: center;
+`
