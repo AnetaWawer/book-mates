473d1586c578c569890942d82c08e21c1161c300
Aneta Wawer <anwawer1@gmail.com>
2023-03-23 23:47:05 +0100

    container stying created

---
+++ src/components/Container.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const ContainerStyles = styled.div `
+  padding-top: 7em;
+  padding-bottom: 7em;
+`
+
+export const NavbarContainer = styled.div `
+  margin-left: 4%;
+  padding-top: 3em;
+`
+
+export const ContainerSize = styled.div `
+  margin: auto;
+  width: 80%;
+
+`
