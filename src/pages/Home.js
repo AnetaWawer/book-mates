d39fb591aafc48ca57ff495d010084d5b153c6b6
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-23 12:12:29 +0100

    feat display book panel with hardcoded list of books

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import BooksBar from "../components/BooksBar";
+import {Container, Typography} from "@mui/material";
 
 
 function Home() {
+    const [books, setBooks] = useState([
+        {
+            id: 1,
+            url: "https://s.lubimyczytac.pl/upload/books/5019000/5019183/1045632-352x500.jpg",
+            title: "Gdzie śpiewają raki",
+            author: "Delia Owens"
+        },
+        {
+            id: 2,
+            url: "https://s.lubimyczytac.pl/upload/books/5019000/5019183/1045632-352x500.jpg",
+            title: "Gdzie śpiewają raki",
+            author: "Delia Owens"
+        },
+        {
+            id: 3,
+            url: "https://s.lubimyczytac.pl/upload/books/5019000/5019183/1045632-352x500.jpg",
+            title: "Gdzie śpiewają raki",
+            author: "Delia Owens"
+        },
+        {
+            id: 4,
+            url: "https://s.lubimyczytac.pl/upload/books/5019000/5019183/1045632-352x500.jpg",
+            title: "Gdzie śpiewają raki",
+            author: "Delia Owens"
+        }
+    ]
+    )
+
     return (
-        <h1>This is the homepage</h1>
+        <Container>
+            <Typography variant="h3" sx={{textAlign: "center"}}>Polecane książki</Typography>
+            <BooksBar books={books}> </BooksBar>
+        </Container>
     );
 }
 
