5ac436741c915393a1d21df10ab7396ecf5e007f
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-24 09:59:52 +0100

    fix height of BookCard

--- src/components/BookHeader.js
+++ src/components/BookHeader.js
@@ -3,6 +3,8 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
+import {CardMedia} from "@mui/material";
+import React from "react";
 
 
 const BookHeader = ({ book }) => {
@@ -10,10 +12,20 @@ const BookHeader = ({ book }) => {
     return (
         <Paper>
             <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <img className="img" src="http://books.google.com/books/content?id=Ii4xwxU2PYsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt={`Cover for ${book.title}`} />
+                <Grid item sm={4}>
+                    {/*<img className="img" src={ book.pictureUrl } alt={`Cover for ${book.title}`} />*/}
+                    <CardMedia
+                        component="img"
+                        image= { book.pictureUrl }
+                        alt={ book.title }
+                        sx={{
+                            p: 1,
+                            height: 300,
+                            width: 200
+                        }}
+                    />
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item sm={8}>
                     <h1 className="book-title">{book.title}</h1>
                     <p className="book-author">{book.author}</p>
                     <Typography component="legend">Ocena</Typography>
