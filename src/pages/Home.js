204b1d0c31dd9ade001e1874b2eab2171e0179f6
Aneta Wawer <anwawer1@gmail.com>
2023-03-29 11:46:22 +0200

    EventsPanel added to main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import ForumPanel from "../components/ForumPanel";
-import {Container, Typography} from "@mui/material";
-import BooksBar from "../components/BooksBar";
-import {useNavigate} from "react-router-dom";
 import axios from "axios";
-import SectionHeader from "../components/SectionHeader";
 import {ContainerSize, ContainerStyles} from "../components/Container.styles";
 import BooksPanel from "../components/BooksPanel";
-import SeeMoreButton from "../components/SeeMoreButton";
+import EventsPanel from "../components/EventsPanel";
 
 function Home() {
     const [books, setBooks] = useState([]);
     const booksHeader = "Polecane książki";
+    const [events, setEvents] = useState([]);
+    const eventsHeader = "Polecane wydarzenia";
     const forumHeader = "Forum";
 
     useEffect(() => {
@@ -25,9 +23,21 @@ function Home() {
         }
     }, [books]);
 
+    useEffect(() => {
+        if (!events.length) {
+            axios.get('http://localhost:8080/api/events?amount=4')
+                .then(response => {
+                    setEvents(response.data);
+                    }
+                )
+                .catch(error => console.log(error));
+        }
+    }, [events]);
+
     return (
         <ContainerSize>
             <BooksPanel books={books} header={booksHeader}/>
+            <EventsPanel events={events} header={eventsHeader}/>
             <ForumPanel header={forumHeader} />
         </ContainerSize>
     );
