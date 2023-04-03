2b524718531efb16a87eb0a9f57653cfc820d2be
Aneta Wawer <anwawer1@gmail.com>
2023-04-03 20:47:57 +0200

    fix bug - corrected time format

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Box, Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
+import moment from "moment";
+
 
 const SingleCard = ({ element }) => {
-
     const navigate = useNavigate();
     return (
         <CardActionArea onClick={element.book ? () => navigate("/events/" + element.id) : () =>navigate("/books/" + element.id)}>
@@ -27,7 +28,7 @@ const SingleCard = ({ element }) => {
                     </Box>
                     <Box>
                         <Typography variant="h6" color="black" sx={{textAlign: "center"}}>
-                            { element.book ? element.eventDate: element.author }
+                            { element.book ? moment(element.eventDate).format('DD.MM.YYYY  HH:mm') : element.author }
                         </Typography>
                     </Box>
                 </CardContent>
