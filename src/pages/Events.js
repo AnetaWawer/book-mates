c48d84c0a2d18f616f3dbd6b030b3e87aec11820
Aneta Wawer <anwawer1@gmail.com>
2023-04-04 17:05:36 +0200

    fixed bug - pagination working correctly

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -2,32 +2,56 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import CardsPanel from "../components/CardsPanel";
 import {ContainerSize} from "../components/Container.styles";
-import {Pagination} from "@mui/material";
+import {TablePagination} from "@mui/material";
 
-function Events(){
+const Events = () =>{
     const eventsHeader = "Wszystkie wydarzenia";
     const [events, setEvents] = useState([]);
     const [eventsCount, setEventsCount] = useState(0);
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(
+        {
+            page: 0,
+            rowsPerPage: 12
+        }
+    );
 
-    const handleChange = (event, value) => {
-        setPage(value);
+    const handlePageChange = (event, newPage) => {
+        setPage({
+            ...page,
+            page: newPage
+        });
+    };
+    const handleChangeRowsPerPage = (event) => {
+        setPage({
+            ...page,
+            rowsPerPage: parseInt(event.target.value, 10),
+            page: 0
+        });
     };
 
     useEffect(() => {
-        if (!events.length) {
-            axios.get('http://localhost:8080/api/events?page' + page)
+            axios.get(`http://localhost:8080/api/events?page=${page.page}&size=${page.rowsPerPage}` )
                 .then(response => {
-                        setEvents(response.data);
+                    setEvents(response.data.content);
+                    setEventsCount(response.data.totalElements);
                     }
                 )
                 .catch(error => console.log(error));
-        }
-    }, [events]);
+    }, [page]);
+
     return (
         <ContainerSize>
             <CardsPanel elements={events} header={eventsHeader}/>
-            <Pagination count={10} showFirstButton showLastButton sx={{ justifyContent:"center", display:'flex'}} onChange={handleChange} />
+            <TablePagination
+                component="div"
+                rowsPerPageOptions={[12,24, 48, 96]}
+                onPageChange={handlePageChange}
+                page={page.page}
+                count={eventsCount}
+                rowsPerPage={page.rowsPerPage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                labelRowsPerPage='Ilość książek na stronie'
+            />
         </ContainerSize>
     );
 };
