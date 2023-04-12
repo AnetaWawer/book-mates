c50ef420626f947468cb6c1c53d6c466abdee5ad
Aneta Wawer <anwawer1@gmail.com>
2023-04-12 10:38:54 +0200

    created topic carousel

--- src/components/organisms/TopicCarousel.js
+++ src/components/organisms/TopicCarousel.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import SingleTopic from "../molecules/SingleTopic";
 import Carousel from "react-material-ui-carousel";
 import {Box, Typography} from "@mui/material";
-import SingleTopic from "./SingleTopic";
 
-const UserTopics = ({ topics }) => {
+const TopicCarousel = ({topics} ) => {
     if (!topics.length){
         return (
             <Box sx={{mt: 5}}>
@@ -13,15 +12,15 @@ const UserTopics = ({ topics }) => {
     }
     if (topics.length>=1) {
         return (
-            <Box sx={{mt: 5}}>
+            <Carousel animation="fade" duration={900} interval={3000}>
                 {
                     topics.map((topic, i) => (
                         <SingleTopic topic={topic} key={i}/>
                     ))
                 }
-            </Box>
+            </Carousel>
         );
     }
 };
 
-export default UserTopics;
+export default TopicCarousel;
