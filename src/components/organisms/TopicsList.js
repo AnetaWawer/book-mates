acbf58e4ddfcd662e2bf7ed23f95d4838739cb51
Aneta Wawer <anwawer1@gmail.com>
2023-04-12 10:40:38 +0200

    refactor -files moved between packages

--- src/components/organisms/TopicsList.js
+++ src/components/organisms/TopicsList.js
@@ -1,17 +1,26 @@
 import SingleTopic from "../molecules/SingleTopic";
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import React from "react";
 
 const TopicsList = ({topics} ) => {
-    return (
-        <Box>
-            {topics.map((topic) => (
-                <Box xs={12} sm={6} xl={3} key={topic.id}>
-                    <SingleTopic topic={topic} />
-                </Box>
-            ))}
-        </Box>
-    );
+    if (!topics.length){
+        return (
+            <Box sx={{mt: 5}}>
+                <Typography>Brak aktywnych wątków</Typography>
+            </Box>
+        )
+    }
+    if (topics.length>=1) {
+        return (
+            <Box>
+                {topics.map((topic) => (
+                    <Box xs={12} sm={6} xl={3} key={topic.id}>
+                        <SingleTopic topic={topic}/>
+                    </Box>
+                ))}
+            </Box>
+        );
+    }
 };
 
 export default TopicsList;
