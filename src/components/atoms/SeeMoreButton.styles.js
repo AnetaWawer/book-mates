3a9f7a68302f6274086d01f7e625f83d2cbd7761
Aneta Wawer <anwawer1@gmail.com>
2023-04-12 14:21:36 +0200

    created styling for SeeMoreButton

---
+++ src/components/atoms/SeeMoreButton.styles.js
@@ -0,0 +1,15 @@
+import {styled} from "@mui/material/styles";
+import {Button} from '@mui/material'
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight'
+
+export const SeeMore = styled(Button)({
+    marginTop:10 ,
+    textAlign:'right',
+    float:'right',
+    color:"inherit",
+})
+
+export const ArrowImage = styled(KeyboardDoubleArrowRightIcon)({
+    fontSize: 20,
+    textAlign:'right'
+})
