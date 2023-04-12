f8d76985298c1362056c068b965b8368d4bd3676
Aneta Wawer <anwawer1@gmail.com>
2023-04-12 13:44:30 +0200

    refactor - div replaced with mui elements and styled

--- src/components/atoms/SectionHeader.js
+++ src/components/atoms/SectionHeader.js
@@ -1,16 +1,16 @@
 import React from "react";
 import {Typography} from '@mui/material'
 import "./SectionHeader.styles"
-import {Header, Title} from "./SectionHeader.styles";
+import {Header, HeaderContainer} from "./SectionHeader.styles";
 
 
 const SectionHeader = ({header}) =>{
     return (
-        <Header className='section-header'>
-            <Title className='section-title'>
-                <Typography component="h2" className="align-center" sx={{fontSize: 50}}>{header}</Typography>
-            </Title>
-        </Header>
+        <HeaderContainer>
+            <Header>
+                <Typography component="h2"  sx={{fontSize: 50}}>{header}</Typography>
+            </Header>
+        </HeaderContainer>
     )
 }
 
