47f883cb33027b70a2f3e103ef14a7efde019c99
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-04-14 15:06:20 +0200

    books page with books from db

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,29 +1,24 @@
 import React, {useEffect, useState} from 'react';
-import { useLocation } from 'react-router-dom';
 import axios from "axios";
+import CardsBar from "../components/organisms/CardsBar";
 
 
 const Books = () => {
-    const location = useLocation();
-    const query = new URLSearchParams(location.search).get('q');
+    const [books, setBooks] = useState([]);
 
-    const [search, setSearch] = useState("");
-    const [bookData, setData] = useState([]);
-    console.log("Starting rendering book page");
 
     useEffect(() => {
-        console.log("Trying to call API");
-        axios.get('http://localhost:8080/api/books/search/' + query)
-            .then(res => setData(res.data))
-            .catch(err => console.log("Error: " + err));
+        axios.get('http://localhost:8080/api/books')
+            .then(response => {
+                    setBooks(response.data);
+                }
+            )
+            .catch(error => console.log(error));
     }, []);
-    console.log(bookData)
 
 
     return (
-        <div>
-
-        </div>
+        <CardsBar elements = {books}  />
     );
 }
 
