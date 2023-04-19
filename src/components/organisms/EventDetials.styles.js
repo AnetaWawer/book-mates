60f83a4e2d1be730253cb787100a2f86e9c2f6ce
Aneta Wawer <anwawer1@gmail.com>
2023-04-19 17:52:36 +0200

    created styling for event sign up modal

---
+++ src/components/organisms/EventDetials.styles.js
@@ -0,0 +1,15 @@
+import {styled} from "@mui/material/styles";
+import {Button, Paper, Typography} from "@mui/material";
+
+export const DetailsWindow = styled(Paper)({
+    backgroundColor:'#eeede7',
+    marginTop:48
+})
+
+export const Text = styled(Typography)({
+    marginBottom:12,
+})
+
+export const EventButton = styled(Button)({
+    color:'inherit',
+})
