4c711e0ee5a6ff0d1ddcab80edfe8da1434c8059
Aneta Wawer <anwawer1@gmail.com>
2023-04-20 16:27:08 +0200

    feature events by bookId implemented

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -5,12 +5,14 @@ import axios from 'axios';
 import {useParams} from "react-router-dom";
 import NewEventModal from "../components/templates/NewEventModal";
 import {Box} from "@mui/material";
+import CardsPanel from "../components/templates/CardsPanel";
 
 
 const Book = () => {
     const [book, setBook] = useState({});
+    const [events, setEvents] = useState([]);
     const { id } = useParams();
-
+    const eventsHeader = "Wydarzenia";
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/books/search/' + id)
@@ -21,11 +23,23 @@ const Book = () => {
             .catch(error => console.log(error));
     }, [id]);
 
+    useEffect(() => {
+        if (!events.length) {
+            axios.get('http://localhost:8080/api/books/'+ id +'/events')
+                .then(response => {
+                    setEvents(response.data);
+                    }
+                )
+                .catch(error => console.log(error));
+        }
+    }, [id]);
+
     return (
         <Box sx={{mt: 5}}>
             <BookDetails book={book} />
             <Description description={book.description} />
             <NewEventModal book={book}/>
+            <CardsPanel elements={events} header={eventsHeader}/>
         </Box>
     );
 };
