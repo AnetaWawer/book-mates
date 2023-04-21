949292f0bbcf4b02b67f1b9507888f1d54b332ed
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-04-21 08:48:32 +0200

    fix CardSPanel

--- src/components/templates/CardsPanel.js
+++ src/components/templates/CardsPanel.js
@@ -2,14 +2,12 @@ import React from 'react';
 import SectionHeader from "../atoms/SectionHeader";
 import CardsBar from "../organisms/CardsBar";
 import {Panel} from "../Container.styles";
-import SeeMoreButton from "../atoms/SeeMoreButton";
 
 const CardsPanel = ({ elements, header }) => {
     return (
         <Panel>
             <SectionHeader header={header} />
             <CardsBar elements = {elements} keyField='id' />
-            <SeeMoreButton />
         </Panel>
     );
 };
