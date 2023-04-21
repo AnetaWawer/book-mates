126ae62bf62a30716c19ac741b903a9cca4ad4b6
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-04-21 08:43:24 +0200

    add import to CardsPanel

--- src/components/templates/CardsPanel.js
+++ src/components/templates/CardsPanel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SectionHeader from "../atoms/SectionHeader";
 import CardsBar from "../organisms/CardsBar";
 import {Panel} from "../Container.styles";
+import SeeMoreButton from "../atoms/SeeMoreButton";
 
 const CardsPanel = ({ elements, header }) => {
     return (
