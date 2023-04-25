90f9fd71fa4d2da180233c1861002a0860bde253
karchal <k.s.chalupka@gmail.com>
2023-04-25 05:49:26 +0200

    fix bug - not unique id in CardsBar component

--- src/components/organisms/CardsBar.js
+++ src/components/organisms/CardsBar.js
@@ -2,14 +2,12 @@ import React from 'react';
 import {Grid} from "@mui/material";
 import SingleCard from "../molecules/SingleCard";
 
-const CardsBar = ( {elements, keyField='id'} ) => {
+const CardsBar = ( {elements} ) => {
     return (
             <Grid container direction={'row'} spacing={4}>
                 {elements.map((element) => (
-                    <Grid item xs={12} sm={6} lg={3} key={element[keyField]}>
-                        <SingleCard
-                            element={element}>
-                        </SingleCard>
+                    <Grid item xs={12} sm={6} lg={3} key={(element.externalId) ? element.externalId : element.id }>
+                        <SingleCard element={element} />
                     </Grid>
                 ))}
             </Grid>
