1de93c6198e1554851dfe75d4260bf4b29e8ff25
Aneta Wawer <anwawer1@gmail.com>
2023-04-26 14:33:29 +0200

    created individual topic page

---
+++ src/pages/Topic.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {MainContainer, Panel} from "../components/Container.styles";
+import {useParams} from "react-router-dom";
+import SectionHeader from "../components/atoms/SectionHeader";
+import SingleTopic from "../components/molecules/SingleTopic";
+import ResponseList from "../components/organisms/ResponseList";
+import ResponseTextField from "../components/molecules/ResponseTextField";
+
+function Topic() {
+    const topicHeader = "Forum";
+    const [topic, setTopic] = useState([]);
+    const [comments, setComments] = useState([]);
+    let { topicId } = useParams();
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/topics/' + topicId)
+            .then(response => {
+                setTopic(response.data);
+                }
+            )
+            .catch(error => console.log(error));
+    }, [topicId]);
+
+    let getComments = () => {
+        axios.get('http://localhost:8080/api/topics/' + topicId + "/comments")
+            .then(response => {
+                    setComments(response.data);
+                }
+            )
+            .catch(error => console.log(error));
+        return comments;
+    }
+
+    useEffect(() => {
+        getComments();
+    }, [comments]);
+
+    return (
+        <MainContainer>
+            <Panel>
+                <SectionHeader header={topicHeader} />
+                <SingleTopic topic={topic} />
+                <ResponseTextField  comments={getComments} />
+                <ResponseList comments={comments} />
+            </Panel>
+        </MainContainer>
+
+    )
+}
+
+
+export default Topic;
