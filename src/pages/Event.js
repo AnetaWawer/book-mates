c24d1fe4a17ea7f441372fda701455f455a736ec
Aneta Wawer <anwawer1@gmail.com>
2023-05-24 14:46:33 +0200

    added user detials

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -8,8 +8,13 @@ import Description from "../components/atoms/Description";
 
 function Event() {
     const [event, setEvent] = useState({});
+    const [userEvents, setUserEvents] = useState([])
+    const [organizer, setOrganizer] = useState([])
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [eventParticipants, setEventParticipants] = useState(0);
     let { eventId } = useParams();
+    let alreadySubscribed = false;
+    let eventOrganizer="";
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/events/' + eventId )
@@ -21,9 +26,33 @@ function Event() {
             .catch(error => console.log(error));
     }, [eventParticipants]);
 
+    useEffect(() => {
+        const checkIfUserIsLoggedId = () => {
+                if (localStorage.getItem('user')!=null){
+                    axios.get(`http://localhost:8080/api/users/profile`)
+                    .then(response => {
+                        setUserEvents(response.data.events)
+                        setOrganizer(response.data.nickname)
+                        setIsLoggedIn(true)
+                    })
+                    .catch(error => console.log(error));
+                }
+            }
+            checkIfUserIsLoggedId()
+    }, []);
+
+    userEvents.forEach((userEvent) => {
+        if (userEvent.eventId == eventId) {
+            alreadySubscribed = true;
+            eventOrganizer=userEvent
+            return;
+        }
+    });
+
+
     return (
         <MainContainer>
-            <EventDetails event={event} getParticipants={setEventParticipants}/>
+            <EventDetails event={event} getParticipants={setEventParticipants} alreadySubscribed={alreadySubscribed} isLoggedIn={isLoggedIn} organizer={organizer}/>
             <Description description={event.description}/>
         </MainContainer>
     );
