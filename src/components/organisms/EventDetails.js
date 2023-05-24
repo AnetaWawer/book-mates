8318fe10cc8b8a5eed856ac614bdb47f063688d2
Aneta Wawer <anwawer1@gmail.com>
2023-05-24 14:48:11 +0200

    added different comment based on event date and user details

--- src/components/organisms/EventDetails.js
+++ src/components/organisms/EventDetails.js
@@ -5,30 +5,56 @@ import EventSignUpModal from "../templates/EventSignUpModal";
 import BookImage from "../atoms/BookImage";
 import {DetailsWindow, Text} from "./EventDetials.styles";
 
+const EventDetails = ({ event, getParticipants, alreadySubscribed, isLoggedIn, organizer }) => {
+    let eventTime = moment(event.eventDate)
+    let currentDate = moment();
 
-const EventDetails = ({ event, getParticipants }) => {
-    return (
-        <DetailsWindow>
-            <Grid container spacing={2} >
-                <Grid item sm={4}>
-                    <BookImage book={event} />
-                </Grid>
-                <Grid item sm={8}>
-                    <Text variant="h4" >{event.title}</Text>
-                    <Text variant="body1">Autor: {event.bookAuthor}</Text>
-                    <Text variant="body1">Książka: {event.bookTitle}</Text>
-                    <Text variant="body1" >Data wydarzenia: {moment(event.eventDate).format('DD.MM.YYYY  HH:mm')} </Text>
-                    <Text variant="body1">Organizator: {event.organizatorName}</Text>
-                    <Text variant="body1">Wolnych miejsc: {event.maxParticipants-event.participants}</Text>
-                    <Text variant="body1">Ilość osób na liście oczekujących: {event.waitingList}</Text>
-                    <Grid container justifyContent="flex-end">
-                        <EventSignUpModal event={event} getParticipants={getParticipants} />
+    function getEventDetails(signForEventDetails) {
+        return (
+            <DetailsWindow>
+                <Grid container spacing={2}>
+                    <Grid item sm={4}>
+                        <BookImage book={event}/>
+                    </Grid>
+                    <Grid item sm={8}>
+                        <Text variant="h4">{event.title}</Text>
+                        <Text variant="body1">Autor: {event.bookAuthor}</Text>
+                        <Text variant="body1">Książka: {event.bookTitle}</Text>
+                        <Text variant="body1">Data
+                            wydarzenia: {moment(event.eventDate).format('DD.MM.YYYY  HH:mm')} </Text>
+                        <Text variant="body1">Organizator: {event.organizatorName}</Text>
+                        <Text variant="body1">Wolnych miejsc: {event.maxParticipants - event.participants}</Text>
+                        <Text variant="body1">Ilość osób na liście oczekujących: {event.waitingList}</Text>
+                        <Grid container justifyContent="flex-end">
+                            {signForEventDetails}
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-        </DetailsWindow>
+            </DetailsWindow>
+        );
+    }
 
-    );
+    if (organizer === event.organizatorName){
+        return (
+            getEventDetails(<Text variant="h6">Jesteś organizatorem tego wydarzenia.</Text>)
+        );
+    }else if (alreadySubscribed) {
+        return (
+            getEventDetails(<Text variant="h6">Jesteś już zapisany na to wydarzenie.</Text>)
+        );
+    } else if (moment(currentDate).isAfter(eventTime)){
+        return (
+            getEventDetails(<Text variant="h6">Wydarzenie już się odbyło.</Text>)
+        );
+    } else if (!isLoggedIn){
+        return (
+            getEventDetails(<Text variant="h6">Aby dołączyć do wydarzenia musisz się zalogować.</Text>)
+        );
+    } else {
+        return (
+            getEventDetails(<EventSignUpModal event={event} getParticipants={getParticipants} />)
+        );
+    }
 }
 
 export default EventDetails;
