60577440d9504cf77e0af9c3bc6252eba1946cce
karchal <k.s.chalupka@gmail.com>
2023-06-14 13:15:30 +0200

    remove unused import

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import {Avatar, Box, Button, Container, Link, Typography, TextField} from "@mui/material";
 import Grid from "@mui/material/Grid";
